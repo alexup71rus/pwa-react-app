9d8b84bf23b52da399773734274a1547e47ba667
alexup71rus <42513019+alexup71rus@users.noreply.github.com>
2019-05-12 00:38:44 +0300

    Initial

---
+++ build/precache-manifest.3ad6709c49060de74a32321cf826a0fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de6c2f053c1e7f572436",
+    "url": "/static/js/main.de6c2f05.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "de6c2f053c1e7f572436",
+    "url": "/static/css/main.a878a639.chunk.css"
+  },
+  {
+    "revision": "7e76482556b703b6f64cb3429db2f173",
+    "url": "/index.html"
+  }
+];
